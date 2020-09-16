81f15b9c8871c03e17cfe2a802fc231b87708fea
asurr876 <asurr876@students.bju.edu>
2020-09-16 16:58:49 -0400

    mvp

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask
+from flask import render_template, request, redirect, url_for
+from gamemain import doCommand, createWorld
 
 app = Flask(__name__)
+hostString = '0.0.0.0'
+w = createWorld('Cluehouse')
+
+# @app.route('/')
+
+# def index():
+#  return render_template('index.html', action = (hostString + ':5000/start'))
+# def login():
+#    if request.method == 'POST':
+#       user = request.form['name']
+#       return redirect(url_for('dashboard',name = user))
+#    else:
+#       user = request.args.get('name')
+#       return render_template('login.html')
+
+@app.route('/game', methods=['POST', 'GET'])
+def game():
+  if request.method == 'POST':
+    return render_template('game.html', message=doCommand(w, request.form['command']))
+  else:
+    name = request.args.get('name')
+    return render_template('game.html', message='welcome to TBAG!')
 
 @app.route('/')
-
 def index():
-
- return 'Hello world'
+  return render_template('index.html')
 
 if __name__ == '__main__':
-
- app.run(debug=True, host='0.0.0.0')
+ app.run(debug=True, host=hostString)
---
+++ gamemain.py
@@ -0,0 +1,51 @@
+from world import World
+from command import Command
+from helptext import helpText
+
+
+def doCommand(w,currentInput):
+    commands = {
+      "go": Command(w.go,True),
+      "look": Command(w.look,False),
+      "whereami": Command(w.whereami,False),
+      "carrying": Command(w.carrying,False),
+      "lookaround": Command(w.lookaround,False),
+      "examine": Command(w.examine,True),
+      "take": Command(w.take,True),
+      "drop": Command(w.drop,True),
+      "save": Command(w.save, False)
+      # "use": Command(w.use,True)
+    }
+    # format input
+    if ' ' in currentInput:
+      command,params = currentInput.split(None, 1)
+    else:
+      command = currentInput
+      params = ""
+    # check for special command
+    if command == 'help':
+      return(helpText)
+    elif command == "exit":
+      # shouldSaveGame = input("Would you like to save before you leave? y or n: ")
+      # if(shouldSaveGame == "y" or shouldSaveGame == "yes"):
+      #   print(commands["save"].useCommand(None))
+      return("Thanks for playing!")
+      exit()
+    else:
+    # execute regular command
+      try:
+        return(commands[command].useCommand(params))
+      except KeyError:
+        return("command '"+ currentInput + "' not recognized. type 'help' for a list of commands")
+
+def createWorld(gameName):
+  # gameName = input("Please enter game folder name (ClueHouse, or custom save folder)")
+  w = None
+  while w == None:
+    try:
+      w = World(gameName)
+    except:
+      print(gameName + " not found.")
+      gameName = input("Please enter game folder name (ClueHouse, or custom save folder)")
+  print(gameName + " game loaded!")
+  return w
